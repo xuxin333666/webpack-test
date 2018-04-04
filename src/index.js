afe87f7fe2239491f2e70701f3c358eaefb2bfdf
xuxin <793307910@qq.com>
2018-04-04 16:26:04 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <h1>Hi, world!</h1>,
+    document.getElementById('root')
+  );
+registerServiceWorker();
